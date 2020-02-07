77fda5048c934d2cd6bcc68e506a06f70375033b
Pooja Yadav <poyadav@ucsd.edu>
2020-02-06 18:11:33 -0800

    Initial Commit

---
+++ flixter/MovieCell.swift
@@ -0,0 +1,29 @@
+//
+//  MovieCell.swift
+//  flixter
+//
+//  Created by Pooja Yadav on 1/29/20.
+//  Copyright © 2020 Pooja Yadav. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ flixter/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flixter
+//
+//  Created by Pooja Yadav on 2/5/20.
+//  Copyright © 2020 Pooja Yadav. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
